2a92b0ebcf065b386c62511cae16dc8b725a5a63
balaji-sivakumar <31851611+balaji-sivakumar@users.noreply.github.com>
2017-09-12 14:17:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
------------------------------------------------------------------------------*/
-
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -10,43 +6,32 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-  
+var MICROSOFT_APP_ID = "7395cd9c-5fa3-4a78-ad7a-7989231387a1";
+var MICROSOFT_APP_PASSWORD = "nYU54bqyjX2BgCEuacbXEAq";
+var LUIS_MODEL_URL="https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/23ecec6e-1bd1-4433-bdd8-a62fadc2d32f?subscription-key=fbd6c82bc4f149eeba4a1ae5d5ffde60&timezoneOffset=0&verbose=true";
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    stateEndpoint: process.env.BotStateEndpoint,
-    openIdMetadata: process.env.BotOpenIdMetadata 
+    appId: MICROSOFT_APP_ID,
+    appPassword: MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
-
-// Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
-
-// Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
-
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
-
-// Main dialog with LUIS
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
 });
+var recognizer = new builder.LuisRecognizer(LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+bot.dialog('Greeting', function (session, args, next) {
+	session.send('welcome to the hotel finder ');
 
-bot.dialog('/', intents);    
-
+	var greetWord = builder.EntityRecognizer.findEntity(args.intent.entities, 'Greet');
+	
+	var name = builder.EntityRecognizer.findEntity(args.intent.entities, 'Name');
+	session.send('greeting entity  %s ,%s:', greetWord.entity,name.entity);
+}).triggerAction({
+	matches: 'Greeting'
+});
